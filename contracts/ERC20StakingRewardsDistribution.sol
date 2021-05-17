a91570ae30366d7734404e405758dc0bc4ab1677
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-05-17 14:43:47 -0700

    feat: allow anyone to add rewards to distribution

--- contracts/ERC20StakingRewardsDistribution.sol
+++ contracts/ERC20StakingRewardsDistribution.sol
@@ -307,7 +307,7 @@ contract ERC20StakingRewardsDistribution {
         lastConsolidationTimestamp = _consolidationTimestamp;
     }
 
-    function addRewards(address _token, uint256 _amount) public onlyOwner {
+    function addRewards(address _token, uint256 _amount) public {
         uint256[] memory _updatedAmounts = new uint256[](rewards.length);
         for (uint32 _i = 0; _i < rewards.length; _i++) {
             address _rewardTokenAddress = rewards[_i].token;
