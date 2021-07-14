814dcbd7362fc816ba64492e4a9b4770c0c3658a
Christopher Pearson <kermit4@gmail.com>
2021-07-14 00:50:27 +0000

    verbose test failure message

--- test.bash
+++ test.bash
@@ -5,5 +5,5 @@ set -e
 find "$@" > /dev/null
 ! find $tdir "$@" -type f -name '*.js' \( -exec node -c {} \; -o \( -print -quit \)  \)  | 
     grep --quiet .
-! find $tdir "$@" -type f -name '*.js' \( -exec truffle test --runner-output-only {} \; -o \( -print -quit \)  \)  | 
-    grep --quiet .
+! find $tdir "$@" -type f -name '*.js' \( -exec truffle test --runner-output-only {} \; -o \( -printf "%p failed\n" -quit \)  \)  | 
+    grep .
