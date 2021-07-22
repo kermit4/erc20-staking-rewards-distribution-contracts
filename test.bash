341a6190aeea0b853ac76de056d68b13da0453f3
Christopher Pearson <kermit4@gmail.com>
2021-07-22 18:31:03 +0000

    use custom tmp dir because truffle doesnt clean up after itself

--- test.bash
+++ test.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 tdir=""
 set -e
+export TMPDIR=`mktemp --tmpdir --directory  yarntestXXXXXXXXXXX`
 [[ $1 ]] || tdir=test
 find $tdir "$@" > /dev/null
 find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 node -c
 find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 truffle test --runner-output-only 
+rm -rf $TMPDIR
