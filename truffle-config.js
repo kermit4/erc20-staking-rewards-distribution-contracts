39ba63e62a3fb408188879903305262b621c5d6c
Christopher Pearson <kermit4@gmail.com>
2021-07-31 15:10:26 +0000

    placeholder, not working

--- truffle-config.js
+++ truffle-config.js
@@ -12,8 +12,18 @@ const configuration = {
         },
     },
     plugins: ["solidity-coverage"],
-};
 
+ networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 1000,
+      gas: 4612388,
+      gasPrice: 25000000000,
+      total_accounts: 20,
+    } }
+
+};
 if (process.argv.indexOf("--gas-report") >= 0) {
     configuration.mocha = {
         reporter: "eth-gas-reporter",
