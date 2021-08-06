c0478dcc8821a64bc2c1ce6798dbd141f1eaf39f
Christopher Pearson <kermit4@gmail.com>
2021-08-06 19:55:47 +0000

    test.bash use ganache, its faster, save output, and remove temp files better

--- test.bash
+++ test.bash
@@ -2,8 +2,10 @@
 tdir=""
 set -e
 export TMPDIR=`mktemp --tmpdir --directory  yarntestXXXXXXXXXXX`
+trap "fuser -swk $TMPDIR/*;rm -rf $TMPDIR" EXIT
 [[ $1 ]] || tdir=test
+ganache-cli  -i 1000 > $TMPDIR/ganache 2> $TMPDIR/ganache.2 &
 find $tdir "$@" > /dev/null
-find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 node -c
-find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 truffle test --runner-output-only 
-rm -rf $TMPDIR
+find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 node -c 
+find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 truffle test --stacktrace-extra --runner-output-only >(tee test.out) 2>(tee test.err)
+echo -e \\a
