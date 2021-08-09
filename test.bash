5c15286efca317a4f5c865f2511e08c0caf40710
Christopher Pearson <kermit4@gmail.com>
2021-08-09 03:26:59 +0000

    revert adding --stacktrace-extra to truffle test, it caused timeouts in truffle

--- test.bash
+++ test.bash
@@ -2,8 +2,8 @@
 tdir=""
 set -e
 export TMPDIR=`mktemp --tmpdir --directory  yarntestXXXXXXXXXXX`
-trap "fuser -swk $TMPDIR/*;rm -rf $TMPDIR" EXIT
 [[ $1 ]] || tdir=test
 find $tdir "$@" > /dev/null
 find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 node -c
-find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 truffle test --stacktrace-extra --runner-output-only >(tee test.out) 2>(tee test.err)
+find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 truffle test --runner-output-only 
+rm -rf $TMPDIR
