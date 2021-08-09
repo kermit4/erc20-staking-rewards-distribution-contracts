845803cbfab939a7fbfd8d66579dec1c613ff06a
Christopher Pearson <kermit4@gmail.com>
2021-08-09 05:02:09 +0000

    test.bash

--- test.bash
+++ test.bash
@@ -2,8 +2,9 @@
 tdir=""
 set -e
 export TMPDIR=`mktemp --tmpdir --directory  yarntestXXXXXXXXXXX`
+trap "fuser -swk $TMPDIR/*;rm -rf $TMPDIR" EXIT
 [[ $1 ]] || tdir=test
 find $tdir "$@" > /dev/null
 find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 node -c
-find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 truffle test --runner-output-only 
+find $tdir "$@" -type f -name '*.js' -print0 | xargs -0 -n 1 truffle test --runner-output-only >(tee test.out) 2>(tee test.err)
 rm -rf $TMPDIR
